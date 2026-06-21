d8cdd8d79627e183c4de09bade457f0db1fa6e29
ahmedabbas52233-a11y <ahmedabbas52233@gmail.com>
2026-06-21 19:26:25 +0500

    fix: files

--- next.config.mjs
+++ next.config.mjs
@@ -29,9 +29,6 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  experimental: {
-    typedRoutes: true,
-  },
   async headers() {
     return [
       {
